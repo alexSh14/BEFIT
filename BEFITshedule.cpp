2166955c7fe61cccd7b2c0fc3dad57f326a3d426
alexSh14 <alex.1486@mail.ru>
2023-05-16 22:48:31 +0700

    read and print FunctionSchedule.h

--- BEFITshedule.cpp
+++ BEFITshedule.cpp
@@ -2,68 +2,23 @@
 //
 
 #include "Header.h"
+#include "FunctionSchedule.h"
 #include <fstream>
 #include <sstream>
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    std::ifstream input_file("schedule.txt");
-    if (!input_file) {
-        std::cerr << "Ошибка открытия файла" << std::endl;
-        return 1;
-    }
 
     std::vector<YogaTraining> yoga_schedule;
     std::vector<ZumbaTraining> zumba_schedule;
 
-    std::string line;
-    std::string category;
-
-    while (std::getline(input_file, line)) {
-        std::istringstream iss(line);
-
-        if (line == "YOGA") {
-            category = "YOGA";
-            continue;
-        }
-        else if (line == "ZUMBA") {
-            category = "ZUMBA";
-            continue;
-        }
-
-        Date date;
-        Time time;
-        Place place;
-
-        char delimiter;
-        iss >> date.day >> delimiter >> date.month >> delimiter >> date.year >> time.hour >> delimiter >> time.minute >> place.name >> place.address;
-
-        if (category == "YOGA") {
-            yoga_schedule.push_back({ date, time, place });
-        }
-        else if (category == "ZUMBA") {
-            zumba_schedule.push_back({ date, time, place });
-        }
+    if (!readScheduleFromFile("schedule.txt", yoga_schedule, zumba_schedule)) {
+        return 1;
     }
 
-    // Вывод расписания тренировок по йоге
-    std::cout << "Расписание тренировок по йоге:" << std::endl;
-    for (const auto& training : yoga_schedule) {
-        std::cout << "Дата: " << training.date.day << "." << training.date.month << "." << training.date.year << std::endl;
-        std::cout << "Время: " << training.time.hour << ":" << training.time.minute << std::endl;
-        std::cout << "Место: " << training.place.name << ", " << training.place.address << std::endl;
-        std::cout << std::endl;
-    }
-
-    // Вывод расписания тренировок по зумбе
-    std::cout << "Расписание тренировок по зумбе:" << std::endl;
-    for (const auto& training : zumba_schedule) {
-        std::cout << "Дата: " << training.date.day << "." << training.date.month << "." << training.date.year << std::endl;
-        std::cout << "Время: " << training.time.hour << ":" << training.time.minute << std::endl;
-        std::cout << "Место: " << training.place.name << ", " << training.place.address << std::endl;
-        std::cout << std::endl;
-    }
+    printSchedule(yoga_schedule, zumba_schedule);
 
     return 0;
 }
+
 
---
+++ FunctionSchedule.h
@@ -0,0 +1,65 @@
+#pragma once
+#include "Header.h"
+#include "FunctionSchedule.h"
+#include <fstream>
+#include <sstream>
+void printSchedule(const std::vector<YogaTraining>& yoga_schedule, const std::vector<ZumbaTraining>& zumba_schedule) {
+    // Вывод расписания тренировок по йоге
+    std::cout << "Расписание тренировок по йоге:" << std::endl;
+    for (const auto& training : yoga_schedule) {
+        std::cout << "Дата: " << training.date.day << "." << training.date.month << "." << training.date.year << std::endl;
+        std::cout << "Время: " << training.time.hour << ":" << training.time.minute << std::endl;
+        std::cout << "Место: " << training.place.name << ", " << training.place.address << std::endl;
+        std::cout << std::endl;
+    }
+
+    // Вывод расписания тренировок по зумбе
+    std::cout << "Расписание тренировок по зумбе:" << std::endl;
+    for (const auto& training : zumba_schedule) {
+        std::cout << "Дата: " << training.date.day << "." << training.date.month << "." << training.date.year << std::endl;
+        std::cout << "Время: " << training.time.hour << ":" << training.time.minute << std::endl;
+        std::cout << "Место: " << training.place.name << ", " << training.place.address << std::endl;
+        std::cout << std::endl;
+    }
+}
+// Функция принимает имя файла, из которого нужно прочитать данные, а также ссылки на два вектора, в которые будут записаны расписания 
+// тренировок по йоге и зумбе соответственно.Функция возвращает true, если чтение файла прошло успешно, и false в противном случае.
+bool readScheduleFromFile(const std::string& file_name, std::vector<YogaTraining>& yoga_schedule, std::vector<ZumbaTraining>& zumba_schedule) {
+    std::ifstream input_file(file_name);
+    if (!input_file) {
+        std::cerr << "Ошибка открытия файла" << std::endl;
+        return false;
+    }
+
+    std::string line;
+    std::string category;
+
+    while (std::getline(input_file, line)) {
+        std::istringstream iss(line);
+
+        if (line == "YOGA") {
+            category = "YOGA";
+            continue;
+        }
+        else if (line == "ZUMBA") {
+            category = "ZUMBA";
+            continue;
+        }
+
+        Date date;
+        Time time;
+        Place place;
+
+        char delimiter;
+        iss >> date.day >> delimiter >> date.month >> delimiter >> date.year >> time.hour >> delimiter >> time.minute >> place.name >> place.address;
+
+        if (category == "YOGA") {
+            yoga_schedule.push_back({ date, time, place });
+        }
+        else if (category == "ZUMBA") {
+            zumba_schedule.push_back({ date, time, place });
+        }
+    }
+
+    return true;
+}
